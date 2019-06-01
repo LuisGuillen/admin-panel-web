4ab148d877e1ae39dbb42fb77e5125c34aa2faac
Luis Guillen <luisr@4mas1.com>
2019-05-31 22:27:31 -0500

    Issue #15

--- api/db.js
+++ api/db.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/admin-panel-web', { useNewUrlParser: true })
+const URI = process.env.MONGODB || 'mongodb://localhost/admin-panel-web';
+
+mongoose.connect(URI, { useNewUrlParser: true })
     .then(() => console.log('Mongo connected'))
     .catch(error => console.error(error));
 
