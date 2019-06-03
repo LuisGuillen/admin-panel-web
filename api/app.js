1c3ca458c47b72e85aede43df3ce5abf62bcb10f
Luis Guillen <luis-rere@hotmail.com>
2019-06-03 10:52:30 -0500

    Issue #21

--- api/app.js
+++ api/app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const graphqlHttp = require('express-graphql');
+const graphQlSchema = require('./graphql/schema');
+const graphQlResolvers = require('./graphql/resolvers');
 require('dotenv').config();
 require('./db');
 
@@ -8,6 +11,7 @@ app.set('port', process.env.PORT || 3000)
 
 app.use(express.json());
 
+// CORS
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'POST,GET,PUT,DELETE');
@@ -19,6 +23,14 @@ app.use((req, res, next) => {
 app.use('/api/users', require('./controllers/users.controller'));
 app.use('/api/cars', require('./controllers/cars.controller'));
 app.use('/api/parts', require('./controllers/parts.controller'));
+
+// gql route
+app.use('/graphql', graphqlHttp({
+        schema: graphQlSchema,
+        rootValue: graphQlResolvers,
+        graphiql: true
+    })
+);
 
 // Public files
 app.use(express.static(path.join(__dirname, 'public')));
